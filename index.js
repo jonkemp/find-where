3030a7c1d016d78fdf895aa02c831355770d41f8
Jonathan Kemp <kempdogg@gmail.com>
2020-03-24 22:21:43 -0400

    Add package-utils.

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const isMatch = (obj, attrs) => {
-	for (const key in attrs) {
-		if (attrs[key] !== obj[key] || !(key in obj)) {
-			return false;
-		}
-	}
-
-	return true;
-};
-
-const matcher = attrs => obj => isMatch(obj, attrs);
+const { matcher } = require('@jonkemp/package-utils');
 
 module.exports = (obj, attrs) => obj.find(matcher(attrs));
