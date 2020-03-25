d97d392a81dee9d206ff310913f5944f3040d511
Jonathan Kemp <kempdogg@gmail.com>
2020-03-24 22:16:56 -0400

    Update unit tests.

--- test.js
+++ test.js
@@ -1,35 +1,37 @@
 const assert = require('assert');
 const findWhere = require('./');
+const isUndefined = obj => obj === void 0;
 
 describe('findWhere', () => {
-	it('should look through the list and return the first value that matches all of the key-value pairs provided', () => {
-		const users = [
-			{ 'user': 'barney',  'age': 36, 'active': true },
-			{ 'user': 'fred',    'age': 40, 'active': false },
-			{ 'user': 'pebbles', 'age': 1,  'active': true }
-		];
-		const expected = { 'user': 'pebbles', 'age': 1, 'active': true };
-		const test = obj => findWhere(users, obj);
+	const list = [{a: 1, b: 2}, {a: 2, b: 2}, {a: 1, b: 3}, {a: 1, b: 4}, {a: 2, b: 4}];
 
-		assert.deepEqual(test({ 'age': 1 }), expected);
-		assert.notDeepEqual(test({ 'age': 2 }), expected);
-		assert.deepEqual(test({ 'user': 'pebbles' }), expected);
-		assert.notDeepEqual(test({ 'user': 'barney' }), expected);
-		assert.deepEqual(test({ 'age': 1,  'active': true }), expected);
-		assert.notDeepEqual(test({ 'age': 1,  'active': false }), expected);
-		assert.deepEqual(test({ 'user': 'pebbles', 'age': 1,  'active': true }), expected);
-		assert.notDeepEqual(test({ 'user': 'pebbles', 'age': 1,  'active': false }), expected);
-		assert.deepEqual(test({ 'active': false }), { 'user': 'fred', 'age': 40, 'active': false });
-		assert.notDeepEqual(test({ 'active': true }), { 'user': 'fred', 'age': 40, 'active': false });
+	it('should look through the list and return the first value that matches all of the key-value pairs provided', () => {
+		let result = findWhere(list, {a: 1});
+
+		assert.deepEqual(result, {a: 1, b: 2});
+		result = findWhere(list, {b: 4});
+		assert.deepEqual(result, {a: 1, b: 4});
 	});
 
-	it('should return undefined if no match is found', () => {
-		const users = [
-			{ 'user': 'barney',  'age': 36, 'active': true },
-			{ 'user': 'fred',    'age': 40, 'active': false },
-			{ 'user': 'pebbles', 'age': 1,  'active': true }
-		];
+	it('undefined when not found', () => {
+		const result = findWhere(list, {c: 1});
 
-		assert(typeof findWhere(users, { 'age': 2 }) === 'undefined');
+		assert.ok(isUndefined(result));
+	});
+
+	it('undefined when searching empty list', () => {
+		const result = findWhere([], {c: 1});
+
+		assert.ok(isUndefined(result));
+	});
+
+	it('uses class instance properties', () => {
+		function TestClass() {
+			this.y = 5;
+			this.x = 'foo';
+		}
+		const expect = {c: 1, x: 'foo', y: 5};
+
+		assert.deepEqual(findWhere([{y: 5, b: 6}, expect], new TestClass()), expect);
 	});
 });
